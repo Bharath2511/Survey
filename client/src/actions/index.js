369e428a12ca64fa4129be7427a281e0be05645e
V S Bharath <bharathcse5t6@gmail.com>
2018-11-24 13:11:47 +0530

    refactoring actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,13 +2,11 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 //fetchuser is action creator
-export const fetchUser = () => {
+export const fetchUser = () =>
   //redux thunk sees if we are returning a function instead of action it
   //will automatically call this function attaching dispatch as function
-  return function(dispatch) {
+  async dispatch => {
     //only after hitting that route we want to dispatch an action
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+    const res = await axios.get("/api/current_user");
+    dispatch({ type: FETCH_USER, payload: res });
   };
-};
