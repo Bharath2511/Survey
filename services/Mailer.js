18214c9453af5aa93c36d016148d5fe7c4ba3d26
V S Bharath <bharathcse5t6@gmail.com>
2018-11-27 10:04:02 +0530

    sendgrid helper class

---
+++ services/Mailer.js
@@ -0,0 +1,8 @@
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
+
+const keys = require("../config/keys");
+
+//class called mailer extending mail classprovided inside the sendgrid library
+//it takes in many objects and spits out one
+class Mailer extends helper.Mail {}
