4d2873a20957fd4077271f190e515b4bab1902a6
V S Bharath <bharathcse5t6@gmail.com>
2018-11-27 10:20:14 +0530

    mailer template

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,3 +6,5 @@ const keys = require("../config/keys");
 //class called mailer extending mail classprovided inside the sendgrid library
 //it takes in many objects and spits out one
 class Mailer extends helper.Mail {}
+
+module.exports = Mailer;
