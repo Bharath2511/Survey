317ec8a662711024bb35ed6df7e80f464e8fa663
V S Bharath <bharathcse5t6@gmail.com>
2018-12-01 18:47:51 +0530

    survey field

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,6 +1,10 @@
 //surveys field contain logic to render a single label and text input
 import React from "react";
 
-export default () => {
-  return <div>SurveyField</div>;
+export default ({ input }) => {
+  return (
+    <div>
+      <input {...input} />
+    </div>
+  );
 };
