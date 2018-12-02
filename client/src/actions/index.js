70402b210675eebc16184035ec3b6c6f491b9552
V S Bharath <bharathcse5t6@gmail.com>
2018-12-02 20:00:20 +0530

    posting to survey

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -20,6 +20,9 @@ export const handleToken = token => async dispatch => {
   //console.log(res.data);
 };
 
-export const submitSurvey = values => {
-  return { type: "submit_survey" };
+//values are the form values
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post("/api/surveys", values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
