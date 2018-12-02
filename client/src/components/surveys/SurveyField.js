d5048370c65aecf81af3cd9a9046c190f1dc5d43
V S Bharath <bharathcse5t6@gmail.com>
2018-12-02 10:01:44 +0530

    validating is awesome

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,12 +1,13 @@
 //surveys field contain logic to render a single label and text input
 import React from "react";
 
-export default ({ input, label }) => {
-  //console.log(input);
+export default ({ input, label, meta: { error, touched } }) => {
+  // console.log(meta);
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
