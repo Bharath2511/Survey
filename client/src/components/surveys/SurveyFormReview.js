bf2bb7e0329d287b3eabf5808c66a1b87cb91d9a
V S Bharath <bharathcse5t6@gmail.com>
2018-12-02 18:48:04 +0530

    persisting form data

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,5 +1,6 @@
 //show user their form preview
 import React from "react";
+import { connect } from "react-redux";
 
 const SurveyFormReview = ({ onCancel }) => {
   return (
@@ -12,4 +13,11 @@ const SurveyFormReview = ({ onCancel }) => {
   );
 };
 
-export default SurveyFormReview;
+function mapStateToProps(state) {
+  //console.log(state);
+  return {
+    formValues: state.form.surveyForm.values
+  };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
