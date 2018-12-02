aedfaa83a4d4c4b777508c39fce0806774f82959
V S Bharath <bharathcse5t6@gmail.com>
2018-12-02 19:08:33 +0530

    finalizing the review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,24 @@
 //show user their form preview
 import React from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
+
+import formFields from "./formFields";
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  //similar to field.name and label
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
